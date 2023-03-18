9ca6a8ce02a39333a89d0e9b08c4f68ed82f4282
Baneist <wjt.recv@qq.com>
2023-03-18 21:13:54 +0800

    Add medical datasets.

---
+++ dataread.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+
+df_medical = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'Medical Cost Personal Datasets.csv'))
+#数值化
+df_medical['sex'] = df_medical['sex'].replace({'female': 0, 'male': 1})
+df_medical['smoker'] = df_medical['smoker'].replace({'yes': 1, 'no': 0})
+df_medical = df_medical.drop(['region'], axis=1)
