4acf194a4c00ba459119569da76bebba00863216
Baneist <wjt.recv@qq.com>
2023-03-18 21:20:53 +0800

    Update dataread.py

--- dataread.py
+++ dataread.py
@@ -6,3 +6,10 @@ df_medical = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'Me
 df_medical['sex'] = df_medical['sex'].replace({'female': 0, 'male': 1})
 df_medical['smoker'] = df_medical['smoker'].replace({'yes': 1, 'no': 0})
 df_medical = df_medical.drop(['region'], axis=1)
+y = []
+x = []
+for index, row in df_medical.iterrows():
+    #x_medical = 
+    t = [i for i in row]
+    y.append(t.pop())
+    x.append(t)
