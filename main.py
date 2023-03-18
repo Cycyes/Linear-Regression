b83a0a438f817f5b9b5a928d271f5315622e5785
Cycyes <92714336+Cycyes@users.noreply.github.com>
2023-03-18 18:37:42 +0800

    update main

--- main.py
+++ main.py
@@ -2,48 +2,45 @@ import argparse
 
 from regression_model import *
 
-def main():
+def main(args):
     x = [[1, 3], [4, 2], [5, 1], [7, 4], [8, 9]]
     y = [1.002, 4.1, 4.96, 6.78, 8.2]
 
+    reg = LR(x=x, y=y, epoch=args.epoch, lr=args.lr, alpha=args.alpha, gamma=args.gamma, beta=args.beta, batch=args.batch, optimization=args.optimization)
     adam = LR(x=x, y=y, epoch=1000, lr=1e-2, optimization="Adam")
     rms = LR(x=x, y=y, epoch=1000, lr=1e-2, optimization="RMSprop")
     none = LR(x=x, y=y, epoch=1000, lr=1e-2, optimization="None")
     m = LR(x=x, y=y, epoch=1000, lr=1e-2, optimization="Momentum")
 
+    reg.fit()
+    '''
     adam.fit()
     rms.fit()
     none.fit()
     m.fit()
+    '''
 
+    print("reg: ", reg.predict([[1, 3]]))
+    '''
     print("adam: ", adam.predict([[1, 3]]))
     print("rms: ", rms.predict([[1, 3]]))
     print("none: ", none.predict([[1, 3]]))
     print("m: ", m.predict([[1, 3]]))
+    '''
 
-
-main()
-'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--augmentation', type=str, default='none',
-                        help="Options are ['none', 'ports', 'ids', 'random', 'dropout']")
-    parser.add_argument('--prob', type=int, default=-1)
-    parser.add_argument('--num_runs', type=int, default=50)
-    parser.add_argument('--num_layers', type=int, default=4)  # 9 layers were used for skipcircles dataset
-    parser.add_argument('--use_aux_loss', action='store_true', default=False)
 
-    parser.add_argument('--verbose', action='store_true', default=False)
+    parser.add_argument('--epoch', type=int, default=10000)
+    parser.add_argument('--lr', type=float, default=1e-2)
+    parser.add_argument('--alpha', type=float, default=0)
+    parser.add_argument('--gamma', type=float, default=0.9)
+    parser.add_argument('--beta', type=float, default=0.9)
+    parser.add_argument('--batch', type=int, default=1)
+    parser.add_argument('--optimization', type=str, default='None',
+                        help="Options are ['None', 'Momentum', 'RMSprop', 'Adam']")
 
-    parser.add_argument('--prob_ablation', action='store_true', default=False, help="Run probability ablation study")
-    parser.add_argument('--num_runs_ablation', action='store_true', default=False,
-                        help="Run number of runs ablation study")
-
-    parser.add_argument('--dataset', type=str, default='limitsone',
-                        help="Options are ['skipcircles', 'triangles', 'lcc', 'limitsone', 'limitstwo', 'fourcycles']")
     args = parser.parse_args()
-
     main(args)
 
-    print('Finished', flush=True)
-    '''
+    print('Finished')
