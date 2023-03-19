88d3683fcc22910357db4f279241d565a6d9aac5
Baneist <wjt.recv@qq.com>
2023-03-19 11:42:55 +0800

    data2

--- dataread.py
+++ dataread.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import numpy as np
 
 def medical_data():
     df_medical = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'Medical Cost Personal Datasets.csv'))
@@ -36,4 +37,28 @@ def train_data_1():
         x.append(t)
     return x, y
 
-print(train_data_1())
+def get_data_2():
+    tmp = []
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), 'datasets', 'CarPrice_Assignment.csv'))
+    col = [i for i in df.columns]
+    for i in col:
+        if isinstance(df[i][0], str):
+            word_arr = []
+            for word in df[i]:
+                if word not in word_arr:
+                    word_arr.append(word)
+            for c in word_arr:
+                tt = []
+                for j in df[i]:
+                    if j == c:
+                        tt.append(1)
+                    else:
+                        tt.append(0)
+                tmp.append(tt)
+        else:
+            tmp.append(list(df[i]))
+    x = []
+    y = tmp.pop()
+    x = [list(i) for i in zip(*tmp)]
+    i = int(len(x) * 0.8)
+    return [x[:i], y[:i]], [x[i:], y[i:]]
